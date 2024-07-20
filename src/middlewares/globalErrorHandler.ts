cbf1bc668886749063d15e6a6b34798e9fdd08b9
devdeepak06 <k.deepak2217@gmail.com>
2024-07-20 16:34:11 +0530

    Add global error handler

---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,18 @@
+// Global error handle;
+import { Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response
+) => {
+  const statusCode = err.statusCode || 500;
+  return res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.env === "development" ? err.stack : "",
+  });
+};
+
+export default globalErrorHandler;
