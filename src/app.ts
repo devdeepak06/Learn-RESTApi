2ce79089329e1c6340fd04f7a5578cf6dff42d10
devdeepak06 <k.deepak2217@gmail.com>
2024-07-20 17:00:54 +0530

    Create User router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 // Routes
 // HTTP methods: GET, POST, PUT, DELETE, PATCH
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.json({ message: "Hello World" });
   // res.send("Hello World");
 });
+
+app.use("/api/users", userRouter);
 
 app.use(globalErrorHandler);
 
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,12 +1,13 @@
 // Global error handle;
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import { config } from "../config/config";
 
 const globalErrorHandler = (
   err: HttpError,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   const statusCode = err.statusCode || 500;
   return res.status(statusCode).json({
